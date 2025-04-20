badaabbadc61de619e5759745308ef6d7c470a9b
amit <amit@openwriteup.com>
2025-04-20 20:14:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket  = "terraformgcpow23"
+    credentials = "location of json file"
+    bucket  = "name of bucket"
     prefix  = "student.01-instance-state"
   }
 }
